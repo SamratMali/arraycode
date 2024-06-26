328ac8be789aa20731f78772caf3d3ff747108a5
SamratMali <samratmali6569@gmail.com>
2024-06-26 23:13:33 +0530

    new java code

---
+++ eveninarray.java
@@ -0,0 +1,17 @@
+public class eveninarray {
+    public static void main(String[] args)
+    {
+            int[] arr = {10,1,9,2,112,3};
+
+            for(int i=0; i<arr.length;i++)
+            {
+                    if(arr[i]%2==0)
+                    {
+                        System.out.println(arr[i]);
+                    }
+            }
+
+
+    }
+
+}

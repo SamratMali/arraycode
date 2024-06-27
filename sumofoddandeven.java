e7ded25fee93a9905907c7074c54da87b4925f50
SamratMali <samratmali6569@gmail.com>
2024-06-27 23:29:26 +0530

    new array codes

--- sumofoddandeven.java
+++ sumofoddandeven.java
@@ -1,3 +1,21 @@
 public class sumofoddandeven {
-    
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,3,4,5,6,};
+        int sumodd=0;
+        int sumeven=0;
+
+        for(int i=0; i<arr.length;i++)
+        {
+            if(arr[i]%2==0)
+            {
+                sumeven+=arr[i];
+            }
+            else{
+            sumodd+=arr[i];
+            }
+        }
+        System.out.println(sumeven);
+        System.out.println(sumodd);
+    }
 }
